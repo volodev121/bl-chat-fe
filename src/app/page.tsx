e806b176196ca093a7bf279630826cc5a1b7bfec
Niklas Betten <ungep@student.kit.edu>
2023-10-04 13:25:21 +0200

    update component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 "use client"; // This is a client component 👈🏽
 
-import useStyles from './components/styles.tsx'
-import { useState } from 'react'
-import ToolTip from './components/toolTip.tsx'
-import { useEffect } from 'react'
-import { getToken } from './utils/authorization.tsx'
-import { getConfig } from './utils/authorization.tsx'
-import ChatWidget from './components/chatWidget.tsx';
+import useStyles from "./components/styles.tsx";
+import { useState } from "react";
+import ToolTip from "./components/toolTip.tsx";
+import { useEffect } from "react";
+import { getToken } from "./utils/authorization.tsx";
+import { getConfig } from "./utils/authorization.tsx";
+import ChatWidget from "./components/chatWidget.tsx";
+import { createRoot } from "react-dom/client";
+import React from "react";
 
 export default function App() {
   const [showChatWidget, setShowChatWidget] = useState(false);
   const [showToolTip, setShowToolTip] = useState(true);
   const styles = useStyles();
   const config = {
-    logo_url: ''
-  }
+    logo_url: "",
+  };
 
   const [hasConfig, setHasConfig] = useState(false);
   const [fetchedConfig, setFetchedConfig] = useState<any>(config);
-  
+
   useEffect(() => {
     const fetchConfig = async () => {
-      const authResponse = await getToken('https://llm-storage.brandslisten.com/api/v1/auth/', {});
+      const authResponse = await getToken(
+        "https://llm-storage.brandslisten.com/api/v1/auth/",
+        {}
+      );
 
       if (authResponse.status === 200) {
         const headers = {
@@ -29,19 +34,23 @@ export default function App() {
             Authorization: authResponse.data.auth_token,
           },
         };
-        
-        const configResponse = await getConfig('https://llm-storage.brandslisten.com/config', {}, headers);
+
+        const configResponse = await getConfig(
+          "https://llm-storage.brandslisten.com/config",
+          {},
+          headers
+        );
         if (configResponse.status === 200) {
           setFetchedConfig(configResponse.data);
           setHasConfig(true);
         } else {
-          console.error('Failed to fetch config:', configResponse.data);
+          console.error("Failed to fetch config:", configResponse.data);
         }
       } else {
-        console.error('Failed to authenticate:', authResponse.data);
+        console.error("Failed to authenticate:", authResponse.data);
       }
     };
-    
+
     fetchConfig();
   }, []);
 
@@ -52,13 +61,37 @@ export default function App() {
 
   return (
     <div className={styles.chatWidget}>
-      {// tooltip
+      {
+        // tooltip
       }
-      {showToolTip ? ( <ToolTip setShowChatWidget={setShowChatWidget} setShowToolTip={setShowToolTip} config={fetchedConfig}/> ) : (<></>) }
-      {//chat widget
-      showChatWidget ? (<ChatWidget setShowChatWidget={setShowChatWidget} setShowToolTip={setShowToolTip} config={fetchedConfig}/> ) : (<></>)
+      {showToolTip ? (
+        <ToolTip
+          setShowChatWidget={setShowChatWidget}
+          setShowToolTip={setShowToolTip}
+          config={fetchedConfig}
+        />
+      ) : (
+        <></>
+      )}
+      {
+        //chat widget
+        showChatWidget ? (
+          <ChatWidget
+            setShowChatWidget={setShowChatWidget}
+            setShowToolTip={setShowToolTip}
+            config={fetchedConfig}
+          />
+        ) : (
+          <></>
+        )
       }
-    </div> )
+    </div>
+  );
 }
 
-window.chatFeWidget = App;
+// make the component available for the window
+const navDomNode = document.querySelector("#bl-chat-widget-bubble-button");
+if (navDomNode != null) {
+  const navRoot = createRoot(navDomNode);
+  navRoot.render(React.createElement(App, {}));
+}
