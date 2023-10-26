5c8d8ba1544176664ceb5f10305e18326b896d33
Hamit Kamberi <hamit@brandslisten.com>
2023-10-26 18:16:29 +0200

    Redesign of the Tooltip Component

--- src/app/components/toolTip.tsx
+++ src/app/components/toolTip.tsx
@@ -1,8 +1,14 @@
-import React, { FC } from 'react';
-import { Grid, Typography, Button } from "@mui/material";
+import React, { FC, useState } from 'react';
+import { Grid, Typography, Button, Input, Paper } from "@mui/material"; // Import Input component
 import useStyles from './styles.tsx';
 import AppleIcon from '@mui/icons-material/Apple';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import InputAdornment from '@mui/material/InputAdornment';
+import SendIcon from '@mui/icons-material/Send';
+import { isEmpty } from 'lodash';
+import ChatIcon from '@mui/icons-material/Chat';
+
+
 
 interface ToolTipProps {
   setShowChatWidget: (flag: boolean) => void;
@@ -12,47 +18,40 @@ interface ToolTipProps {
 
 const ToolTip: FC<ToolTipProps> = ({ setShowChatWidget, setShowToolTip, config }) => {
   const styles = useStyles();
+  const [inputValue, setInputValue] = useState(''); // State to manage input value
+
   const handleClick = () => {
     setShowChatWidget(true);
     setShowToolTip(false);
   };
+
+  const [inputMessage, setInputMessage] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value); // Update input value when it changes
+  };
+
   const termsLink = <a href={config.terms_of_service_link || '#'} className={styles.termsConditionsLink}>Terms&Conditions</a>;
 
   return (
     <>
     <Grid className={styles.toolTip}>
-      <Grid className={styles.toolTipBlack} item>
-        <Grid item className={styles.logo}>
-          <img 
-            src={config.logo_url} 
-            style={{ height: '40px', width: '40px', margin: '2em' }}
-            role="presentation"
-            alt=""
-          />
-        </Grid>
-        <Typography className={styles.toolTipText}>{config.widget_headline}</Typography>
-      </Grid>
       <Grid item className={styles.toolTipWhite}>
-        <Button
-          variant="contained"
-          className={styles.findCrmButton}
-          sx={{
-            backgroundColor: '#D02DF5',
-            textTransform: 'none',
-            width: 270,
-            ':hover': {
-              backgroundColor: '#aa32c5',
-              borderColor: '#0062cc',
-              boxShadow: 'none',
-            },
-          }}
-          endIcon={<ArrowForwardIosIcon style={{ fontSize: '15px', marginLeft:'5em' }} />}
-          onClick={() => handleClick()}
-        >
-          {config.cta_button_prompt}
-        </Button>
+        <Input
+            type='text'
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Let's start the converstation!"
+            className={styles.inputStyle}
+            disableUnderline
+            endAdornment={
+              <Button style= {{borderRadius: '100%', borderColor: '#ABABBE', width: '32px', height:'32px'}}>
+                <SendIcon style={{ color: 'black' }} />
+              </Button>
+            }
+          />
       </Grid>
-      <Typography className={styles.termsConditions}>By continuing you agree to {termsLink}</Typography>
+        <Typography className={styles.termsConditions}>By continuing you agree to {termsLink}</Typography>
     </Grid>
     </>
   );
